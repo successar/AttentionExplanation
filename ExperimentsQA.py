4745481bc5370baf8bde0f1fa656c6594119a06e
Sarthak Jain <successar@gmail.com>
2019-03-30 20:50:53 -0400

    Config

--- ExperimentsQA.py
+++ ExperimentsQA.py
@@ -7,7 +7,7 @@ def train_dataset(dataset, config) :
     try :
         config = configurations_qa[config](dataset)
         trainer = Trainer(dataset, config=config, _type=dataset.trainer_type)
-        trainer.train(dataset.train_data, dataset.test_data, n_iters=8, save_on_metric=dataset.save_on_metric)
+        trainer.train(dataset.train_data, dataset.test_data, n_iters=25, save_on_metric=dataset.save_on_metric)
         evaluator = Evaluator(dataset, trainer.model.dirname)
         _ = evaluator.evaluate(dataset.test_data, save_results=True)
         return trainer, evaluator

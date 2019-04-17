a4a2414b2cccdb6edcead5a9eb87e49dba38f7b7
Sarthak Jain <successar@gmail.com>
2019-04-17 15:08:03 -0400

    Instructions to process MIMIC data

--- preprocess/MIMIC/Extract_discharge_summaries.sql
+++
@@ -1,5 +0,0 @@
-select subject_id, hadm_id, string_agg(text , ' ' order by chartdate)
-from mimiciii.noteevents 
-where category = 'Discharge summary' 
-and iserror is null
-group by subject_id, hadm_id;
